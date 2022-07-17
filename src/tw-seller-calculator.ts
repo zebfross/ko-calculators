1936e5f01df020295bbbd4f5500500dbd573544d
Zeb Fross <zebfross@hotmail.com>
2022-07-16 18:57:09 -0700

    add comments; income calculation; card types

--- src/tw-seller-calculator.ts
+++ src/tw-seller-calculator.ts
@@ -3,24 +3,40 @@
 declare var ko: KnockoutStatic;
 
 class TradelineModel {
+    app: AppViewModel;
     type: KnockoutObservable<string>;
     limit: KnockoutObservable<number>;
     age: KnockoutObservable<number>;
+    spots: KnockoutObservable<number>;
     perfectPaymentHistory: KnockoutObservable<boolean>;
 
     displayLimit: KnockoutComputed<string>;
 
     displayAge: KnockoutComputed<string>;
+    maxSpots: KnockoutComputed<number>;
 
     incomeForRate(this: TradelineModel, rate: number) {
-        return (this.limit() * 0.015 + this.age() * 25) * rate;
+        if (!this.perfectPaymentHistory() || this.limit() < 3000)
+            return 0;
+        return (Math.max(275, (this.limit() * 0.015 + this.age() * 25)) * rate) * this.spots();
     }
 
-    constructor() {
-        this.type = ko.observable("Chase");
-        this.limit = ko.observable(2000);
-        this.age = ko.observable(2);
-        this.perfectPaymentHistory = ko.observable<boolean>(false);
+    comments: KnockoutComputed<string>;
+
+    income: KnockoutComputed<number>;
+
+    constructor(app=null, type="", limit=5000, age=5, perfectPaymentHistory=true) {
+        this.app = app;
+        this.type = ko.observable(type);
+        this.limit = ko.observable(limit);
+        this.age = ko.observable(age);
+        this.spots = ko.observable(this.app.maxSpotsForCardType(type));
+        this.type.subscribe(() => {
+            let max = this.app.maxSpotsForCardType(this.type());
+            if (max < this.spots())
+                this.spots(max);
+        })
+        this.perfectPaymentHistory = ko.observable<boolean>(perfectPaymentHistory);
 
         this.displayLimit = ko.computed(() => {
             // @ts-ignore
@@ -30,21 +46,39 @@ class TradelineModel {
         this.displayAge = ko.computed(() => {
             return this.age() + " years";
         });
+
+        this.maxSpots = ko.computed(() => {
+            return this.app.maxSpotsForCardType(this.type());
+        })
+
+        this.income = ko.computed(() => {
+            return this.incomeForRate(0.4);
+        });
+
+        this.comments = ko.computed(() => {
+            if (!this.perfectPaymentHistory())
+                return "We only accept cards with perfect payment history.";
+
+            if (this.limit() < 3000)
+                return "We only accept cards with a limit of $3000 or more.";
+
+            return "";
+        });
     }
 }
 
 class AppViewModel {
-    tradelines = ko.observableArray([new TradelineModel()]);
+    tradelines = ko.observableArray([]);
 
     addTradeline = function (this: AppViewModel) {
-        this.tradelines.push(new TradelineModel());
+        this.tradelines.push(new TradelineModel(this));
     };
     removeTradeline: () => void;
 
     incomeForRate(this: AppViewModel, rate: number) {
         // @ts-ignore
         let income = this.tradelines().reduce(function (acc: number, tradeline: TradelineModel) {
-            return acc + tradeline.incomeForRate(rate) + 1;
+            return acc + tradeline.incomeForRate(rate);
         }, 0)
             .toLocaleString()
         return (
@@ -53,6 +87,31 @@ class AppViewModel {
     }
 
     income: KnockoutComputed<string>;
+
+    cardTypes = {
+        "Chase": 2,
+        "Discover": 1,
+        "Capital One": 2,
+        "US Bank": 2,
+        "Bank of America": 2
+    }
+
+    cardNames = Object.keys(this.cardTypes);
+
+    maxSpotsForCardType(type: string) {
+        let max = this.cardTypes[type];
+        if (max === undefined) {
+            return 1;
+        }
+        return max;
+    }
+
+    buttonText: KnockoutComputed<string> = ko.computed(() => {
+        if (this.tradelines().length === 0) {
+            return "Add a Tradeline";
+        }
+        return "Add Another Tradeline";
+    });
 
     constructor() {
         var _this = this;
